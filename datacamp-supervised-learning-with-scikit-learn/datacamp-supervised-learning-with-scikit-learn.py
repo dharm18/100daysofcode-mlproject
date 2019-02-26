0b0c8ad07365b071348e25d01cc7641571ca8713
dharm18 <vishdharm.181910@gmail.com>
2019-02-26 00:57:36 +0000

    Preprocessing code. categorical to numerical data for keras api.

--- datacamp-supervised-learning-with-scikit-learn/datacamp-supervised-learning-with-scikit-learn.py
+++ datacamp-supervised-learning-with-scikit-learn/datacamp-supervised-learning-with-scikit-learn.py
@@ -405,3 +405,48 @@ print("Tuned ElasticNet l1 ratio: {}".format(gm_cv.best_params_))
 print("Tuned ElasticNet R squared: {}".format(r2))
 print("Tuned ElasticNet MSE: {}".format(mse))
 
+
+######PRocessing 
+
+# Import pandas
+import pandas as pd
+
+# Read 'gapminder.csv' into a DataFrame: df
+df = pd.read_csv('gapminder.csv')
+
+# Create a boxplot of life expectancy per region
+df.boxplot('life', 'Region', rot=60)
+
+# Show the plot
+plt.show()
+
+##################
+
+# Create dummy variables: df_region
+df_region = pd.get_dummies(df)
+
+# Print the columns of df_region
+print(df_region.columns)
+
+# Create dummy variables with drop_first=True: df_region
+df_region = pd.get_dummies(df, drop_first=True)
+
+# Print the new columns of df_region
+print(df_region.columns)
+
+########################
+
+# Import necessary modules
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import cross_val_score
+
+# Instantiate a ridge regressor: ridge
+ridge = Ridge(alpha=0.5,normalize=True)
+
+# Perform 5-fold cross-validation: ridge_cv
+ridge_cv = cross_val_score(ridge, X,y,cv=5)
+
+# Print the cross-validated scores
+print(ridge_cv)
+
+
