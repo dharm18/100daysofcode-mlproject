b858387fe1883b7a937057c974e8bb3d1a13116a
dharm18 <vishdharm.181910@gmail.com>
2019-03-23 21:53:15 +0000

    Chapter one

---
+++ datacamp-deep-learning-in-python/code.py
@@ -0,0 +1,4 @@
+# Comparing neural network models to classical regression models
+### Which of the models in the diagrams has greater ability to account for interactions?
+
+# MOre hidden layers, greater the interaction, greater the ability for interpretation
