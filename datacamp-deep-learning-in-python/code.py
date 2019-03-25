13b017d346fb8c8c6a15f117427a4f9aae3d7010
dharm18 <vishdharm.181910@gmail.com>
2019-03-25 20:01:23 +0000

    Data Camp Code.

--- datacamp-deep-learning-in-python/code.py
+++ datacamp-deep-learning-in-python/code.py
@@ -293,3 +293,99 @@ model.compile(optimizer='adam', loss='mean_squared_error')
 
 # Verify that model contains information from compiling
 print("Loss function: " + model.loss)
+
+############################### Fitting the model
+
+# Import necessary modules
+import keras
+from keras.layers import Dense
+from keras.models import Sequential
+
+# Specify the model
+n_cols = predictors.shape[1]
+model = Sequential()
+model.add(Dense(50, activation='relu', input_shape = (n_cols,)))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1))
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Fit the model
+model.fit(predictors, target)
+
+#####################################################################################################
+# Classification using DL 
+#####################################################################################################
+
+# Last steps in classification models
+
+# Import necessary modules
+import keras
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.utils import to_categorical
+
+# Convert the target to categorical: target
+target = to_categorical(df.survived)
+
+# Set up the model
+model = Sequential()
+
+# Add the first layer
+model.add(Dense(32, activation='relu', input_shape=(n_cols,)))
+
+# Add the output layer
+model.add(Dense(2, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Fit the model
+model.fit(predictors, target)
+
+### Making predictions
+
+# Specify, compile, and fit the model
+model = Sequential()
+model.add(Dense(32, activation='relu', input_shape = (n_cols,)))
+model.add(Dense(2, activation='softmax'))
+model.compile(optimizer='sgd', 
+              loss='categorical_crossentropy', 
+              metrics=['accuracy'])
+model.fit(predictors, target)
+
+# Calculate predictions: predictions
+predictions = model.predict(pred_data)
+
+# Calculate predicted probability of survival: predicted_prob_true
+predicted_prob_true = predictions[:,1]
+
+# print predicted_prob_true
+print(predicted_prob_true)
+
+## Changing optimization parameters
+
+# Import the SGD optimizer
+from keras.optimizers import SGD
+
+# Create list of learning rates: lr_to_test
+lr_to_test = [.000001, 0.01, 1]
+
+# Loop over learning rates
+for lr in lr_to_test:
+    print('\n\nTesting model with learning rate: %f\n'%lr )
+    
+    # Build new model to test, unaffected by previous models
+    model = get_new_model()
+    
+    # Create SGD optimizer with specified learning rate: my_optimizer
+    my_optimizer = SGD(lr=lr)
+    
+    # Compile the model
+    model.compile(optimizer=my_optimizer, loss='categorical_crossentropy')
+    
+    # Fit the model
+    model.fit(predictors, target)
+    
+
