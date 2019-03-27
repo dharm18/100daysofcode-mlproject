dbcc76bc465313669086fabb708626671f6f19ba
dharm18 <vishdharm.181910@gmail.com>
2019-03-27 00:37:02 +0000

    DataCamp code

---
+++ datacamp-statistical-thinking-in-python-part-1/code.py
@@ -0,0 +1,29 @@
+# Plotting a histogram of iris data
+
+# Import plotting modules
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Set default Seaborn style
+sns.set()
+
+# Plot histogram of versicolor petal lengths
+plt.hist(versicolor_petal_length)
+
+# Show histogram
+plt.show()
+
+# Axis labels!
+
+# Plot histogram of versicolor petal lengths
+_ = plt.hist(versicolor_petal_length)
+
+# Label axes
+_ = plt.xlabel('petal length (cm)')
+_ = plt.ylabel('count')
+
+
+# Show histogram
+plt.show()
+
+
